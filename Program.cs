95bdda9ae5e8ddbe60ef9c3e5776a284f7d9826d
lautaro anazgo <lautaroanazgo@gmail.com>
2025-06-05 23:35:43 -0300

    Ejercicio 1 resuelto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Ingrese el numero que desea invertir:");
+string entrada = Console.ReadLine();
+int numeroAInvertir = 0;
+bool esNumero = int.TryParse(entrada, out numeroAInvertir);
+int numeroInvertido = 0;
+
+if (esNumero)
+{
+    if (numeroAInvertir > 0)
+    {
+        while (numeroAInvertir > 0)
+        {
+            int digito = numeroAInvertir % 10;           // Extraer el último dígito (ej: 123 % 10 = 3)
+            numeroInvertido = (numeroInvertido * 10) + digito; // Construir el número invertido (ej: 0*10+3=3; 3*10+2=32; 32*10+1=321)
+            numeroAInvertir /= 10;                       // Eliminar el último dígito (ej: 123 / 10 = 12)
+        }
+    }
+}
+
+Console.WriteLine($"El numero invertido es: {numeroInvertido}");
